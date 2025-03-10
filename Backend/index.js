7ff6eff17956a549ddb6f6cc78b66fbb3c58ff2a
Gaurav-Attri <attrigaurav60@gmail.com>
2025-03-10 15:14:21 +0530

    Installed body-parser package and setup the index.js file, basic template of routes

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser')
+const userRouter = require('./routes/user');
+const todoRouter = require('./routes/todo');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/user', userRouter);
+app.use('/todo', todoRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(`The Server is listening on PORT ${process.env.PORT}`);
+});
