f23188459ac13fcd77067c5bc28613a08a8d29d3
Gaurav-Attri <attrigaurav60@gmail.com>
2025-03-10 14:54:46 +0530

    Installed dotenv and setup the db code

---
+++ Backend/db/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config()
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI);
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    todosCreated: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Todo'
+    }]
+});
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    Status: Boolean,
+});
+
+const User =  mongoose.Model("User", User);
+const Todo = mongoose.Model("Todo", Todo);
+
+module.exports = {
+    User,
+    Todo
+}
